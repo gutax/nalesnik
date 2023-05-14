86424cc7082a5c897d9659892a266bd6cfdf1f90
macko <macko@LAPTOP-GTN4HOE5>
2023-05-14 17:39:25 +0200

    Add project files.

---
+++ HelloWorld2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Zawijasz naleśniki w rulon czy trójkąt?");
+        string odpowiedz = Console.ReadLine();
+
+        if (odpowiedz.ToLower() == "rulon")
+        {
+            Console.WriteLine("Debil.");
+        }
+        else if (odpowiedz.ToLower() == "trojkat")
+        {
+            Console.WriteLine("Gratulacje!");
+        }
+        else
+        {
+            Console.WriteLine("wpisz rulon albo trojkat(bez polskich znakow)");
+        }
+
+        Console.WriteLine("koniec");
+        Console.ReadKey();
+    }
+}
